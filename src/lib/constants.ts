789ac19d7a7c67e1ce572781e5dacb217088fd4c
Sicheng Chen <csc0330@hotmail.com>
2025-07-27 01:10:41 +0800

    chore: update application version to 0.6.0

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 // Application constants
-export const APP_VERSION = "0.5.0"; 
+export const APP_VERSION = "0.6.0"; 
 export const APP_NAME = "Progressive Cast";
 export const APP_DESCRIPTION = "Progressive Cast - A Podcast Player";
